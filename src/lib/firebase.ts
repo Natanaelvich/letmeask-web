ccc0aee68dc62b4f963f18feaf510a7395c36874
Natanael Lima <taelima1997@gmail.com>
2021-06-22 11:57:21 -0300

    update firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -17,5 +17,7 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-export const auth = firebase.auth();
-export const database = firebase.database();
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { auth, database, firebase };
